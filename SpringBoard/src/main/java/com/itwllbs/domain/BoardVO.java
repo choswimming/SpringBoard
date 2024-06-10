327ec19d590e9592e3c018a438790425b9291411
choswimming <tndud001209@naver.com>
2024-06-10 10:53:43 +0900

    #6 게시판 글쓰기-DAO 처리 완료(+테스트)

---
+++ SpringBoard/src/main/java/com/itwllbs/domain/BoardVO.java
@@ -0,0 +1,21 @@
+package com.itwllbs.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private Integer bno;
+	private String title;
+	private String content;
+	private String writer;
+	private	Timestamp regdate;
+	private int viewcnt;
+	
+	
+	
+	
+	
+	
+}
---
+++ SpringBoard/src/test/java/com/itwillbs/controller/BoardDAOTest.java
@@ -0,0 +1,49 @@
+package com.itwillbs.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.itwillbs.persistence.BoardDAO;
+import com.itwllbs.domain.BoardVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+		locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"}
+		)
+public class BoardDAOTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
+
+	// DAO 객체 주입
+	@Autowired
+	private BoardDAO bdao;
+	
+	@Test
+	public void 글쓰기테스트() throws Exception {
+		logger.debug(" 글스기 테스트 동작 실행 ");
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle("테스트글1");
+		vo.setContent("테스트 내용 1111");
+		vo.setWriter("테스트1");
+		
+		
+		bdao.create(vo);
+//		try {
+//			bdao.create(vo);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		
+	}
+	
+	
+}
